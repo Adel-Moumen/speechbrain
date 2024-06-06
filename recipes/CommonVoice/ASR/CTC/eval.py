fd5b6b7a53da697da341905950ddd46acda88576
Adel Moumen <adelmoumen.pro@gmail.com>
2024-06-06 12:04:51 +0200

    duration

--- recipes/CommonVoice/ASR/CTC/eval.py
+++ recipes/CommonVoice/ASR/CTC/eval.py
@@ -279,7 +279,7 @@ def dataio_prepare(hparams, tokenizer):
     # We also sort the validation data so it is faster to validate
     test_data = test_data.filtered_sorted(
         sort_key="duration",
-        key_max_value={"duration": 60},
+        key_max_value={"duration": 30},
     )
 
     datasets = [train_data, valid_data, test_data]
